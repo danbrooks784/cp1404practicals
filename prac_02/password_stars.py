686c6f521a8d20d7205ee44c2726f937a65cfd63
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-22 22:58:38 +1000

    Refactor program into main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,10 @@
 def main():
+    minimum_length = 6
     password = input("Password: ")
+    while len(password) < minimum_length:
+        print("Password is too short")
+        password = input("Password: ")
     print("*" * len(password))
 
 
 main()
-
