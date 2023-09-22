905e8acce44907f6826d0c437960f833c30a7370
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-22 23:02:05 +1000

    Refactor password input into different function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,15 @@
 def main():
+    password = get_password()
+    print("*" * len(password))
+
+
+def get_password():
     minimum_length = 6
     password = input("Password: ")
     while len(password) < minimum_length:
         print("Password is too short")
         password = input("Password: ")
-    print("*" * len(password))
+    return password
 
 
 main()
