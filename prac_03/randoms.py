e64155f3fbfbbad0aeada6c27f55f17f474474e4
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-29 22:36:45 +1000

    Add answer to second question

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -8,3 +8,12 @@ CP1404 Prac 3 - Randoms
 # What was the smallest number you could have seen, what was the largest?
 # 5, 20
 
+
+# What did you see on line 2?
+# 9
+
+# What was the smallest number you could have seen, what was the largest?
+# 3, 9
+
+# Could line 2 have produced a 4?
+# No, since the function moves in steps of 2
