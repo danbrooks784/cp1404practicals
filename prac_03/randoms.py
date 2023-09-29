a18ca9a2bfe5dd5d308dcc9ba898def6ae6a13b4
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-30 09:34:20 +1000

    Refactor to use comments

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,28 +1,29 @@
 """
 CP1404 Prac 3 - Randoms
-
-Q: What did you see on line 1?
-14
-
-Q: What was the smallest number you could have seen, what was the largest?
-5 and 20
-
-
-Q: What did you see on line 2?
-9
-
-Q: What was the smallest number you could have seen, what was the largest?
-3 and 9
-
-Q: Could line 2 have produced a 4?
-No, since the function moves in steps of 2.
-
-
-Q: What did you see on line 3?
-5.438799824665393
-
-Q: What was the smallest number you could have seen, what was the largest?
-2.5 and 5.5
 """
+
+# What did you see on line 1?
+# 14
+
+# What was the smallest number you could have seen, what was the largest?
+# 5 and 20
+
+
+# What did you see on line 2?
+# 9
+
+# What was the smallest number you could have seen, what was the largest?
+# 3 and 9
+
+# Could line 2 have produced a 4?
+# No, since the function moves in steps of 2
+
+
+# What did you see on line 3?
+# 5.438799824665393
+
+# What was the smallest number you could have seen, what was the largest?
+# 2.5 and 5.5
+
 import random
 print(random.uniform(1, 100))
