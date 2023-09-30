f7a4e2f1b4c65d2c15eb7c0df2af186c3a8f2fbc
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-30 21:19:00 +1000

    Add answer to question 3, add strip method to question 2 name reading

--- prac_03/files.py
+++ prac_03/files.py
@@ -10,6 +10,13 @@ out_file.close()
 
 # 2.
 in_file = open("name.txt", 'r')
-name = in_file.read()
-in_file.close()
+name = in_file.read().strip()
 print(f"Your name is {name}")
+in_file.close()
+
+# 3.
+in_file = open("numbers.txt", 'r')
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+print(f"{number1} + {number2} = {number1 + number2}")
+in_file.close()
