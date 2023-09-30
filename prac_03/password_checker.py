5e700240ad130d483689459e14c37554482b8a65
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-30 22:06:24 +1000

    Refactor main function to use f-strings

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -2,26 +2,28 @@
 CP1404 Prac 3 - Password Checker
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+MIN_LENGTH = 5
+MAX_LENGTH = 15
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
     """Program to get and check a user's password."""
     print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters and contain:")
     print("\t1 or more uppercase characters")
     print("\t1 or more lowercase characters")
     print("\t1 or more numbers")
+
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
+
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
         password = input("> ")
+
     print(f"Your {len(password)}-character password is valid: {password}")
 
 
