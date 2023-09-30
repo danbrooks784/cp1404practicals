9678c858677231cad0ceb7c6af516b9ddb502730
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-09-30 21:13:55 +1000

    Add answer to question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,3 +8,8 @@ out_file = open("name.txt", 'w')
 print(name, file=out_file)
 out_file.close()
 
+# 2.
+in_file = open("name.txt", 'r')
+name = in_file.read()
+in_file.close()
+print(f"Your name is {name}")
