16693d0724312a1a8f44a18f033f93a5a449d633
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-01 09:48:08 +1000

    Reformat final print statement to use f-strings

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -9,4 +9,4 @@ while not is_finished:
         is_finished = True
     except ValueError:
         print("Please enter a valid integer.")
-print("Valid result is:", result)
+print(f"Valid result is: {result}")
