cd089e3ef6e3838a0bbe12c495200c38672c287e
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-06 16:42:14 +1000

    Remove TODO from comment

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -42,7 +42,7 @@ print(numbers)
 large_numbers = [number for number in numbers if number > 9]
 print(large_numbers)
 
-# TODO: (more advanced) use a list comprehension and the join string method
+# (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
 last_names = [name_pair[1] for name_pair in [name.split(' ') for name in full_names]]
