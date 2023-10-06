6dc76c75cb885cc7a20b17977df39a73bd2a5740
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-06 17:00:21 +1000

    Add formatted pick printing

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,4 +3,18 @@ CP1404 Prac 4 - Quick Picks
 """
 import random
 
+NUMBERS_PER_LINE = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
 number_of_picks = int(input("How many quick picks? "))
+
+for i in range(number_of_picks):
+    picks = []
+    for j in range(NUMBERS_PER_LINE):
+        pick = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        while pick in picks:
+            pick = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        picks.append(pick)
+        picks.sort()
+    print("{:2} {:2} {:2} {:2} {:2} {:2}".format(*picks))
