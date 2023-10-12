f7e189ee69336f22ae876f6144563142c5ed04b6
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 08:28:42 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,11 +1,10 @@
 """
-CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
+CP1404 Prac 5 - State Names
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
 
