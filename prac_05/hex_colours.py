97a74ecc082ff533e9d5745967d7736b52c119a4
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 08:38:00 +1000

    Add hex code dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,7 @@
+"""
+CP1404 Prac 5 - Hex Colours
+"""
+
+COLOUR_TO_CODE = {"Apricot": "#fbceb1", "CarolinaBlue": "#56a0d3", "GhostWhite": "#f8f8ff", "HarvestGold": "#da9100",
+                  "JazzberryJam": "#a50b5e", "Lilac": "#c8a2c8", "Mint": "#3eb489", "PearlyPurple": "#b768a2",
+                  "Pumpkin": "#ff7518", "Sinopia": "#cb410b", "Vanilla": "#f3e5ab", "YellowGreen": "#9acd32"}
