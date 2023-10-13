38b5dec1fe62ddbc745bb3d5c69dd255ee2fd820
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 22:51:19 +1000

    Sort word list and display like first output example

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -12,3 +12,8 @@ for word in words:
         word_to_count[word] += 1
     except KeyError:
         word_to_count[word] = 1
+
+words = list(word_to_count.keys())
+words.sort()
+for word in words:
+    print(f"{word}: {word_to_count[word]}")
