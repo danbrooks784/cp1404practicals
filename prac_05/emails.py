f2631770b341abc5d5856c34d8af11f540b7bc61
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 23:18:58 +1000

    Add check that name is correct and subsequent input

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,18 +6,23 @@ Actual:
 
 
 def main():
+    """Store user emails and names in a dictionary."""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        email_to_name[email] = extract_name(email)
+        name = extract_name_from_email(email)
+        choice = input(f"Is your name {name}? (Y/n) ").upper()
+        if choice != "y" and choice != "":
+            name = input("Name: ")
+        email_to_name[email] = name
         email = input("Email: ")
     print(email_to_name)
 
 
-def extract_name(email):
+def extract_name_from_email(email):
     """Extract the user's name from provided email."""
-    name = email.split('@')[0]
-    name_parts = name.split('.')
+    name = email.split('@')
+    name_parts = name[0].split('.')
     name = ' '.join(name_parts)
     return name.title()
 
