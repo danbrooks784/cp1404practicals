7dc0a91eb544ffd2ced683eaf2955ce084c8b5fa
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 22:56:56 +1000

    Fix formatting on print statement

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,11 +1,12 @@
 """
 CP1404 Prac 5 - Word Occurences
 Estimate: 10 minutes
-Actual: 9 minutes
+Actual: 11 minutes
 """
 
 text = input("Text: ")
 words = text.strip().split(' ')
+
 word_to_count = {}
 for word in words:
     try:
@@ -17,4 +18,4 @@ words = list(word_to_count.keys())
 words.sort()
 max_word_length = max(len(word) for word in words)
 for word in words:
-    print(f"{word:{max_word_length}}: {word_to_count[word]}")
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
