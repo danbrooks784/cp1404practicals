8151c9cce7a780e219f9c978dc043bcd1f27b4bf
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-13 23:11:57 +1000

    Add function to extract name from email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,6 +4,22 @@ Estimate: 15 minutes
 Actual:
 """
 
-email = input("Email: ")
-while email != "":
+
+def main():
+    email_to_name = {}
     email = input("Email: ")
+    while email != "":
+        email_to_name[email] = extract_name(email)
+        email = input("Email: ")
+    print(email_to_name)
+
+
+def extract_name(email):
+    """Extract the user's name from provided email."""
+    name = email.split('@')[0]
+    name_parts = name.split('.')
+    name = ' '.join(name_parts)
+    return name.title()
+
+
+main()
