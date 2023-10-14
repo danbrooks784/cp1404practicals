d05d2d1b90f4326eb7c10f50881fbcb75209f400
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-14 14:51:57 +1000

    Add program to repository

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,11 @@
+"""
+CP1404 Prac 5 - Wimbledon
+Estimated time: 30 minutes
+Actual time:
+"""
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    """Read the wimbledon winners file and display processed data from it."""
