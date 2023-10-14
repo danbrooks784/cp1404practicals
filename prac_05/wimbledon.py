86dbeb70cddb802ad3c3161b8a162204d2c0663f
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-14 15:06:24 +1000

    Refactor records variable name, add preliminary champion win count processing

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,12 +9,23 @@ FILENAME = "wimbledon.csv"
 
 def main():
     """Read the wimbledon winners file and display processed data from it."""
-    lines = []
+    records = []
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
-            lines.append(line.strip().split(','))
-    print(lines)
+            records.append(line.strip().split(','))
+    display_champions(records)
+
+
+def display_champions(records):
+    """Print the champions and how many times they have won."""
+    champion_to_win_count = {}
+    for record in records:
+        try:
+            champion_to_win_count[record[2]] += 1
+        except KeyError:
+            champion_to_win_count[record[2]] = 1
+    print(champion_to_win_count)
 
 
 main()
