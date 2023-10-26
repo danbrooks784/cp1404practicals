e49a8a244544bd267478e06b85dff51d0041897b
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-26 20:23:20 +1000

    Add example languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,3 +3,10 @@ CP1404 Prac 6 - Language
 Estimated time: 15 minutes
 Actual time:
 """
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
