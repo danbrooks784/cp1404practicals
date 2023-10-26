63ed29d5e45d1d195cf717f99bad2b018b1c94f8
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-26 20:19:51 +1000

    Create methods for class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,16 @@
 CP1404 Prac 6 - Programming Language class module.
 """
 
+
+class ProgrammingLanguage:
+    """Programming Language object."""
+
+    def __init__(self, typing="", reflection=True, year=0):
+        """Construct a programming language."""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return if a language is dynamic."""
+        return self.typing == "Dynamic"
