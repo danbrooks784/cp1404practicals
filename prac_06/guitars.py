2c52f25e317049e5ca2fed12ec2cf28540eb252e
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-27 20:46:48 +1000

    Add enumerate loop and final print statements

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,7 +8,7 @@ from guitar import Guitar
 
 
 def main():
-    """"""
+    """Make a list of guitar objects, then print the list."""
     guitars = []
     print("My guitars!")
     name = input("Name: ")
@@ -19,6 +19,14 @@ def main():
         guitars.append(guitar)
         print(f"{guitar} added.\n")
         name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "vintage" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10.2f} ({vintage_string})")
 
 
 main()
