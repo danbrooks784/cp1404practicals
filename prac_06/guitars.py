1a5399adf5adb5300ad3ab41c6c1400dc8155e51
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-27 20:38:23 +1000

    Add guitar info input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,3 +4,21 @@ Estimated time: 15 minutes
 Actual time:
 """
 
+from guitar import Guitar
+
+
+def main():
+    """"""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+
+main()
