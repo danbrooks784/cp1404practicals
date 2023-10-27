be6faf1512d23fae3586948a654e65b9750a42b7
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-10-27 20:57:15 +1000

    Fix formatting on vintage string

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,7 +1,7 @@
 """
 CP1404 Prac 6 - Guitars
 Estimated time: 15 minutes
-Actual time: 21 minutes
+Actual time: 22 minutes
 """
 
 from guitar import Guitar
@@ -25,8 +25,8 @@ def main():
 
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
-        vintage_string = "vintage" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10.2f} ({vintage_string})")
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year:4}), worth ${guitar.cost:10.2f} {vintage_string}")
 
 
 main()
