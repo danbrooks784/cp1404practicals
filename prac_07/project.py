a27360b1a32fdf720a172cebfb24c2a8ff238cc7
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-04 22:09:54 +1000

    Add files to repository

---
+++ prac_07/project.py
@@ -0,0 +1,5 @@
+"""
+CP1404 Prac 7 - Project Module
+Estimated time: 60 minutes
+Actual time:
+"""
---
+++ prac_07/project_management.py
@@ -0,0 +1,5 @@
+"""
+CP1404 Prac 7 - Project Management
+Estimated time: 60 minutes
+Actual time:
+"""
