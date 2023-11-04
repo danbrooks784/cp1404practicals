4e7796f9bc5ef041e82060196b88ec5da2820e9a
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-04 21:44:01 +1000

    Load guitars file and create Guitar objects

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,3 +1,16 @@
 """
 CP1404 Prac 7 - My Guitars
 """
+
+from guitar import Guitar
+import csv
+
+with open("guitars.csv", "r") as in_file:
+    guitars = []
+    reader = csv.reader(in_file)
+    for row in reader:
+        row[1] = int(row[1])
+        row[2] = float(row[2])
+        print(row)
+        guitar = Guitar(row[0], row[1], row[2])
+        guitars.append(guitar)
