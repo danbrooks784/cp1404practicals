e2a335cd1b835bbfbc696b58455162eac4c087bb
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-10 21:03:34 +1000

    Add build method to app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,3 +3,16 @@ CP1404 Prac 8 - Miles to Kilometers Converter
 Estimated time: 60 minutes
 Actual time:
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class MilesKilometersConverter(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+
+MilesKilometersConverter().run()
