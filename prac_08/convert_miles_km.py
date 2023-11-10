6c9ef2a7629fa05b018a9df4af155bc129d78a2b
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-10 21:28:07 +1000

    Add program functionality

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,12 +7,26 @@ Actual time:
 from kivy.app import App
 from kivy.lang import Builder
 
+CONVERSION_RATE = 1.609
+
 
 class MilesKilometersConverter(App):
+    """Convert a distance in kilometers to miles."""
     def build(self):
+        """Build Converter app from KV file."""
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
+
+    def handle_increment(self, amount):
+        """Increase or decrease the input field by 1."""
+        result = float(self.root.ids.input_field.text) + amount
+        self.root.ids.input_field.text = str(result)
+
+    def handle_conversion(self, distance_km):
+        """Convert the distance to miles."""
+        distance_miles = float(distance_km) * CONVERSION_RATE
+        self.root.ids.output_label.text = str(distance_miles)
 
 
 MilesKilometersConverter().run()
