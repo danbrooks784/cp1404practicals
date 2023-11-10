77875316b69283889f0d01f77b026cd464aa0c52
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-10 22:06:42 +1000

    Add build functionality

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,3 +3,20 @@ CP1404 Prac 8 - Dynamic Labels
 Estimated time: 30 minutes
 Actual time:
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class DynamicLabels(App):
+    def __init__(self):
+        super().__init__()
+        self.names = ["Daniel", "James", "Emma", "Henry", "Charlotte", "Lucas"]
+
+    def build(self):
+        self.title = "Dynamic Labels Exercise"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        return self.root
+
+
+DynamicLabels().run()
