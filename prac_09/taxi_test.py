57aa099d17918b2b03046fb7cf8dd5ef9c5f128e
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-17 15:59:20 +1000

    Add answers to questions 4 and 5, reformat fare display

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -12,4 +12,12 @@ my_taxi.drive(40)
 
 # 3.
 print(f"Taxi details: {my_taxi}")
-print(f"Current fare: ${my_taxi.get_fare()}")
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+# 4.
+my_taxi.start_fare()
+my_taxi.drive(100)
+
+# 5.
+print(f"Taxi details: {my_taxi}")
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
