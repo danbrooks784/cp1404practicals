0648d4317c23d75fc2f8300273c0b9a64a29fcc0
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-17 18:21:27 +1000

    Complete the play method and add missing docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -12,7 +12,14 @@ class Band:
         self.musicians = []
 
     def __str__(self):
+        """Return the Band name and every Musician in the Band."""
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
     def add(self, musician):
+        """Add a new Musician to the Band."""
         self.musicians.append(musician)
+
+    def play(self):
+        """Display the play method for every Musician."""
+        for musician in self.musicians:
+            print(musician.play())
