e619facf53176fe9a16c451fb70ed605748fd889
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-17 16:24:55 +1000

    Add overriden drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,6 +3,7 @@ CP1404 Prac 9 - Unreliable Car class
 """
 
 from car import Car
+from random import randint
 
 
 class UnreliableCar(Car):
@@ -12,3 +13,10 @@ class UnreliableCar(Car):
         """Initialise an Unreliable Car instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = randint(0, 100)
+        if random_number < self.reliability:
+            super().drive(distance)
+        else:
+            return 0
