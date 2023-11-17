f4c9d1ced09f29a7b7ac052ede4ca2a76985923d
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-17 21:28:30 +1000

    Add program to repository

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Prac 9 - Taxi Simulator
+Estimated time: 60 minutes
+Actual time:
+"""
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    pass
+
+
+main()
