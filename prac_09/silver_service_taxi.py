235e0b2e9e1247f173f54ed063c4710bf54400d8
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-17 16:42:55 +1000

    Add flagfall as a class variable, override __str__ and get_fare methods to include this

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -7,8 +7,17 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Specialised version of a Taxi that raises the price per km by the fanciness passed in."""
+    flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
         """Initialise a Silver Service Taxi object."""
         super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string like a Taxi but with the flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip with the flagfall."""
+        return self.flagfall + super().get_fare()
