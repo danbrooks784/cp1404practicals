9a51aed35871ad8b1d45987e2ee6bb65744b0bb4
danbrooks784 <daniel.brooks@my.jcu.edu.au>
2023-11-25 13:22:34 +1000

    Modify program to print page title, summary and URL

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,8 +7,10 @@ import wikipedia
 query = input(">>> ")
 while query != "":
     try:
-        if query in wikipedia.search(query):
-            print(wikipedia.summary(query))
+        wiki_page = wikipedia.page(query, auto_suggest=False)
+        print(wiki_page.title)
+        print(wiki_page.summary)
+        print(wiki_page.url)
     except wikipedia.exceptions.DisambiguationError:
         print("Disambiguation error.")
     except wikipedia.exceptions.PageError:
